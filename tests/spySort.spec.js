96d2662dd1bba51912e65c77d7667fd02c73c185
Ian Renauld <ian@qaclique.com>
2025-06-02 08:24:59 -0400

    Add framework and initial test files

---
+++ tests/spySort.spec.js
@@ -0,0 +1,16 @@
+describe("Sort Mutual Funds Market Leaders page on Globe And Mail website", () => {
+  before(async () => {
+    await browser.url("https://www.theglobeandmail.com/investing/markets/funds/market-leaders");
+  });
+
+  it("should sort on 'Last' column", async () => {
+    const mock = await browser.mock("https://globeandmail.pl.barchart.com/module/dataTable.json", { method: "post" });
+    // Click on the 'Last' column header to sort the table
+    await $("th[field='lastPrice']").click();
+    await browser.pause(1000);
+    // Show the request body
+    console.log(JSON.parse(mock.calls[0].postData));
+    // Show the response body
+    console.log(mock.calls[0].body);
+  });
+});
