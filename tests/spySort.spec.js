5b9a5eeaba067625ecdd518465c585ad44c96513
Ian Renauld <ian@qaclique.com>
2025-09-03 16:12:40 -0400

    Update test for demo frontend

--- tests/spySort.spec.js
+++ tests/spySort.spec.js
@@ -1,15 +1,22 @@
-describe("Spy Sort Mutual Funds Market Leaders page on Globe And Mail website", () => {
+import assert from 'node:assert';
+
+describe('Spy Sort Mutual Funds Market Leaders', () => {
   before(async () => {
-    await browser.url("https://www.theglobeandmail.com/investing/markets/funds/market-leaders");
+    await browser.url("");
+    // Wait for the data loading spinner to go away
+    await $("div.loading-overlay > div.spinner").waitForDisplayed({reverse: true});
   });
 
-  it("should sort on 'Last' column", async () => {
-    const mock = await browser.mock("https://globeandmail.pl.barchart.com/module/dataTable.json", { method: "post" });
-    // Click on the 'Last' column header to sort the table
-    await $("th[field='lastPrice']").click();
-    await browser.pause(1000);
+  it('should sort on "Last Price" column', async () => {
+    const mock = await browser.mock('http://localhost:5174/api/funds', { method: 'post' });
+    // Click on the 'Last Price' column header to sort the table
+    await $('th[data-testId="lastPrice"]').click();
+    // Wait a bit for the request to be captured
+    await browser.pause(500);
     // Show the request body
-    console.log(JSON.parse(mock.calls[0].postData));
+    const requestBody = JSON.parse(mock.calls[0].postData);
+    console.log(requestBody);
+    assert(requestBody.orderBy === 'lastPrice', `Expected orderBy to be 'lastPrice', but got ${requestBody.orderBy}`);
     // The response is also spied, but since checking response is an API test, we will do nothing with it here
   });
 });
