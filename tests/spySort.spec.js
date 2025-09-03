8fbfe5068be3fb0410cc8265636126807c5c7984
Ian Renauld <ian@qaclique.com>
2025-09-03 16:33:53 -0400

    Add tests for the indicator

--- tests/spySort.spec.js
+++ tests/spySort.spec.js
@@ -1,22 +1,37 @@
 import assert from 'node:assert';
 
-describe('Spy Sort Mutual Funds Market Leaders', () => {
+describe('Spy Sort Mutual Funds Leaders', () => {
+  let requestBody;
+
   before(async () => {
     await browser.url("");
     // Wait for the data loading spinner to go away
     await $("div.loading-overlay > div.spinner").waitForDisplayed({reverse: true});
   });
 
-  it('should sort on "Last Price" column', async () => {
+  it('should fire API call to sort on "lastPrice" field', async () => {
     const mock = await browser.mock('http://localhost:5174/api/funds', { method: 'post' });
-    // Click on the 'Last Price' column header to sort the table
     await $('th[data-testId="lastPrice"]').click();
     // Wait a bit for the request to be captured
     await browser.pause(500);
-    // Show the request body
-    const requestBody = JSON.parse(mock.calls[0].postData);
+    requestBody = JSON.parse(mock.calls[0].postData);
     console.log(requestBody);
-    assert(requestBody.orderBy === 'lastPrice', `Expected orderBy to be 'lastPrice', but got ${requestBody.orderBy}`);
-    // The response is also spied, but since checking response is an API test, we will do nothing with it here
+    assert(requestBody.orderBy === 'lastPrice',
+      `Expected orderBy to be 'lastPrice', but got ${requestBody.orderBy}`);
+  });
+
+  it('should sort ascending in the API request', async () => {
+    assert(requestBody.orderDir === 'asc', `Expected orderDir to be 'asc', but got ${requestBody.orderDir}`);
+  });
+  // The response is also spied, but since checking response is an API test, we will do nothing with it here
+
+  it('should display the sort indicator on "Last Price" column', async () => {
+    await $('[data-testid="sort-indicator-lastPrice"]')
+      .waitForDisplayed({timeoutMsg: 'Expected: Sort indicator is displayed in the "Last Price" column, but it was not'});
+  });
+
+  it('should display the sort indicator in the right direction', async () => {
+    const direction = await $('[data-testid="sort-indicator-lastPrice"]').getAttribute('data-sort-direction');
+    assert(direction === 'asc', `Expected sort direction to be 'asc', but got ${direction}`);
   });
 });
