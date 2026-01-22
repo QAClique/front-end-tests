dc971aa096e2d50c53ccde4905975dba2e21798e
Ian Renauld <ian@qaclique.com>
2026-01-22 13:48:37 -0500

    Remove unneeded base page object

--- playwright/pages/mutualFundsTable.page.js
+++ playwright/pages/mutualFundsTable.page.js
@@ -1,9 +1,12 @@
-import Page from './page.js';
-
-class MutualFundsTablePage extends Page {
+class MutualFundsTablePage {
   columnNames = ['Symbol', 'Name', 'Last Price', 'Change', '% Change', 'AUM', 'Trade Time'];
   columnMappings = [];
 
+  constructor(page) {
+    this.page = page;
+  }
+
+  get getSpinner()         { return this.page.locator('div.loading-overlay > div.spinner'); }
   get numberOfFundsField() { return this.page.locator('[data-testid="number-of-funds-select"]'); }
   get headerRow()          { return this.page.locator('thead > tr'); }
   get columnHeaders()      { return this.page.locator('thead > tr > th'); }
@@ -15,14 +18,21 @@ class MutualFundsTablePage extends Page {
    * @param {string} path - The path to the page
    */
   async open(path) {
-    await super.open(path);
-    await this.getSpinner().waitFor({ state: 'hidden' });
+    await this.page.setViewportSize({ width: 1440, height: 1024 });
+    await this.page.goto(path);
+    await this.getSpinner.waitFor({ state: 'hidden' });
     const headers = await this.columnHeaders.all();
     this.columnMappings = await Promise.all(
       headers.map((el) => el.getAttribute('data-testid'))
     );
   }
 
+  /**
+   * Gets the column header element by name for further actions
+   *
+   * @param {string} name of the column header
+   * @returns {Playwright.Locator} column header element
+   */
   getColumnHeader(name) {
     return this.page.locator(`th[data-testid="${name}"]`);
   }
@@ -69,16 +79,35 @@ class MutualFundsTablePage extends Page {
     return cells[index].textContent();
   }
 
+  /**
+   * Gets the class attribute for a specific column and row (by using index values for columns)
+   *
+   * @param {number} index of the column
+   * @param {number} row number
+   * @returns {string} the class attribute for the specified column and row
+   */
   async getClassForColumnRow(index, row) {
     const rows = await this.fundRows.all();
     const cells = await rows[row].locator('td').all();
     return cells[index].getAttribute('class');
   }
 
+  /**
+   * Get the index value of a column by its label
+   *
+   * @param {string} label (name) of the column
+   * @returns {number} the index of the column
+   */
   getColumnIndexByLabel(label) {
     return this.columnNames.findIndex((column) => column === label);
   }
 
+  /**
+   * Formats a raw date (in seconds since epoch) to YYYY-MM-DD format
+   *
+   * @param {number} rawDate in seconds since epoch
+   * @returns {string} formatted date in YYYY-MM-DD format
+   */
   formatDate(rawDate) {
     const date = new Date(rawDate * 1000);
     const year = date.getFullYear();
